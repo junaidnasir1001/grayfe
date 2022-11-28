4ab6464adf2ff1f628cdacb6be247ef2b3b2f5e9
wasif mayo <119296531+wasifmayo@users.noreply.github.com>
2022-11-29 00:35:29 +0500

    meta description
    width=device-width, initial-scale=1 replacing with this sentense

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="Want to grow your <b>Business</b>? <b>Develop</b> your Website by Grayfe company we have best <b>Professional</b> Developer in Lahore Pakistan.">
 
         </title>Web Development & Designing - Digital Marketing-Grayfe</title>
 
